9bbb0617313b4c5e38fd3d283e9e6496b647fc07
Allan Barbosa <allan.barbosa@dextra-sw.com>
2019-04-09 18:18:12 -0300

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const authorization = (secretOrPublicKey, req, res, next) => {
+  const auth = req.headers.authorization;
+
+  if (!auth) return res.status(401).send({ error: "No token provided" });
+
+  const parts = auth.split(" ");
+
+  if (!parts.length === 2)
+    return res.status(401).send({ error: "Token error" });
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).send({ error: "Token malformatted" });
+  }
+
+  jwt.verify(token, secretOrPublicKey, (err, decoded) => {
+    if (err) return res.status(401).send({ error: "Token invalid" });
+
+    return next();
+  });
+};
+
+module.exports = authorization;
